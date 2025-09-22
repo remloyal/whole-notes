f4cb016bb2a022809d6d5c5ee94840989cf2b9a4
remloyal <remloyal@163.com>
2025-09-22 23:44:54 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import 'unfonts.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
